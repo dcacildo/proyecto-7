df35f099757b979de49c88af0c21471bfd438ae0
Daniel C <daniel.cacildo15@gmail.com>
2025-04-22 16:10:47 -0600

    cambios9

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ st.header('Data viewer')
 
 car_data = pd.read_csv(
     'vehicles_us.csv')  # leer los datos
-hist_button = st.checkbox('Construir histograma')  # crear un botón
 
 st.dataframe(car_data)
+
+hist_button = st.checkbox('Construir histograma')  # crear un botón
+
 
 if hist_button:
     # al hacer clic en el botón
